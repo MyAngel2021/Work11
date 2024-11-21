523f56be26e1e01561fc2d9088c112aba130779b
МойАнгел2021 <megerlv@yandex.ru>
2024-11-21 21:42:07 +1000

    исправлены ошибки

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,33 @@
-const WebSocked = require('ws') ;
-const wss = new WebSocked.Server({port:8080});
-let clients =[];
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+let clients = [];
 wss.on('connection', (ws) => {
-    console.log('Новый клиент подключился!');
-   clients.push(ws);
-    ws.send(JSON.stringify({
-        type: 'system',
-        content: 'Добро пожаловать в чат!'
-    }));
-    ws.on('message', (message) => {
-        let parseMessage;
-        try {
-            parseMessage = JSON.parse(message);
-            console.log(`Получено сообщение: ` , parseMessage);
-        } catch (error) {
-            console.log("Произошла ошибка при обработке сообщения:" + error);
-            return;
-        }
-    wss.clients.forEach(client => {
-            if(client.readyState === WebSocked.OPEN) {
-                client.send(JSON.stringify(parseMessage));
-            }
-        })
-});
-ws.on('close', function() {
-   clients = clients.filter((client) => client!=ws);
+  console.log('Новый клиент подключился!');
+  clients.push(ws);
+  ws.send(
+    JSON.stringify({
+      type: 'system',
+      content: 'Добро пожаловать в чат!',
+    }),
+  );
+  ws.on('message', (message) => {
+    let parseMessage;
+    try {
+      parseMessage = JSON.parse(message);
+      console.log(`Получено сообщение: `, parseMessage);
+    } catch (error) {
+      console.log('Произошла ошибка при обработке сообщения:' + error);
+      return;
+    }
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(parseMessage));
+      }
+    });
+  });
+  ws.on('close', function () {
+    clients = clients.filter((client) => client != ws);
     console.log('клиент отключился!');
   });
-    
 });
 console.log('сервер запущен на localhost:8080');
--- frondend/chat.js
+++ frondend/chat.js
@@ -1,12 +1,13 @@
-const chat =document.getElementById('chat');
+const chat = document.getElementById('chat');
 const messageForm = document.getElementById('messageForm');
-const messageInput =document.getElementById('messageInput');
+const messageInput = document.getElementById('messageInput');
+const messageInputName = document.getElementById('messageInputName');
 
 const socket = new WebSocket('ws://localhost:8080');
 
 socket.onopen = (e) => {
-    console.log('Соединение успешно!');
-}
+  console.log('Соединение успешно!');
+};
 socket.onmessage = (event) => {
   /* console.log('Получили сообщение: ', event.data); */
   const message = JSON.parse(event.data);
@@ -14,41 +15,48 @@ socket.onmessage = (event) => {
   console.log(message);
   if (message.type === 'system') {
     messageElement.classList.add('system-message');
-  }
-  messageElement.textContent = message.content;
-  chat.appendChild(messageElement);
-  chat.scrollTop = chat.scrollHeight;
-}
-socket.onclose = (event) => {
-    if (event.wasClean) {
-        console.log(`Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+    const userNik = JSON.parse(localStorage.getItem('user'));
+    if (userNik) {
+      console.log(userNik);
+      messageElement.textContent = message.content + ' ' + userNik;
+      messageInputName.value = userNik;
     } else {
-        console.log('Соединение преравно!');
+      messageElement.textContent = message.content;
     }
-}
+  } else if (message.user) {
+    messageElement.textContent = message.user + ':' + message.content;
+  } else {
+    messageElement.textContent = message.content;
+  }
+
+  chat.appendChild(messageElement);
+  messageElement.scrollIntoView();
+};
+socket.onclose = (event) => {
+  if (event.wasClean) {
+    console.log(
+      `Соединение закрыто чисто, код=${event.code} причина=${event.reason}`,
+    );
+  } else {
+    console.log('Соединение преравно!');
+  }
+};
 socket.onerror = (error) => {
-    console.log(`Ошибка ${error.message}`);
-}
-document.getElementById('sendButton').addEventListener ('click',
-    function(){
-        const messageInput = document.getElementById('messageInput');
-        const messag = {           
-            type:'user',
-            content: messageInput.value.trim()
-        };
-        socket.send(JSON.stringify( messag));
-        messageInput.value='';
-    });
-/*
-messageForm.onsubmit = (e) => {
-    e.preventDefalt();
-    if (messageInput.value ) {
-        const message = {
-            type:'user',
-            content: messageInput.value
-        };
-        socket.send(JSON.stringify(message));
-        console.log(message);
-        messageInput.value='';
+  console.log(`Ошибка ${error.message}`);
+};
+document.getElementById('sendButton').addEventListener('click', function () {
+  const messageInput = document.getElementById('messageInput');
+  const messageInputName = document.getElementById('messageInputName');
+  if (messageInput.value.trim() > '') {
+    const messag = {
+      type: 'user',
+      user: messageInputName.value.trim(),
+      content: messageInput.value.trim(),
+    };
+    socket.send(JSON.stringify(messag));
+    messageInput.value = '';
+    if (messageInputName.value.trim() > '') {
+      localStorage.setItem('user', JSON.stringify(messageInputName.value));
     }
-}*/
+  }
+});
